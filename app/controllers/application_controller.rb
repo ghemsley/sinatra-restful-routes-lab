22f6fd75ff5ca4ce68c82187c0ec555db77aeb4f
ghemsley <ghemsley@protonmail.ch>
2021-02-01 14:25:51 -0600

    commit tree 8c8674771b404016db44051b7276038b5e9d83ee

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,47 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/recipes/new' do
+    erb :new
+  end
 
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :recipe
+  end
+
+  post '/recipes' do
+    recipe = Recipe.new
+    recipe.name = params[:name]
+    recipe.ingredients = params[:ingredients]
+    recipe.cook_time = params[:cook_time]
+    recipe.save
+    redirect "/recipes/#{recipe.id}"
+  end
+
+  patch '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name] if params[:name]
+    @recipe.ingredients = params[:ingredients] if params[:ingredients]
+    @recipe.cook_time = params[:cook_time] if params[:cook_time]
+    @recipe.save
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  delete '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.destroy
+    redirect '/recipes'
+  end
+
+  # code actions here!
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :recipes
+  end
 end
